23c088ab65dcd005fc6f9c5b71d47109439e712b
Dayane <dayane.lima.castro@gmail.com>
2018-11-22 20:50:47 -0200

    58: Criando um módulo de funcionalidade

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -6,29 +6,32 @@ import { MaterializeModule } from 'angular2-materialize';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { CursosComponent } from './cursos/cursos.component';
-import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
-import { CursosService } from './../../../servicos/src/app/cursos/cursos.service';
-import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
-//import { routing } from './app.routing';
+import { CursosModule } from './cursos/cursos.module';
 import { AppRoutingModule } from './app.routing.module';
+
+// import { CursosComponent } from './cursos/cursos.component';
+// import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
+// import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
+//import { routing } from './app.routing';
+// import { CursosService } from './cursos/cursos.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    CursosComponent,
-    CursoDetalheComponent,
-    CursoNaoEncontradoComponent
+    // CursosComponent,
+    // CursoDetalheComponent,
+    // CursoNaoEncontradoComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    //routing
+    CursosModule,
     AppRoutingModule
+    //routing
   ],
-  providers: [CursosService],
   bootstrap: [AppComponent]
+  //providers: [CursosService],
 })
 export class AppModule { }
