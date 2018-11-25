8c6348107fe76bcf55c083fd4280f02bd76a3a20
Dayane <dayane.lima.castro@gmail.com>
2018-11-25 09:42:26 -0200

    60: Rotas Filhas

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CursosModule } from './cursos/cursos.module';
 import { AppRoutingModule } from './app.routing.module';
+import { AlunosModule } from './alunos/alunos.module';
 
 // import { CursosComponent } from './cursos/cursos.component';
 // import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
@@ -28,6 +29,7 @@ import { AppRoutingModule } from './app.routing.module';
     BrowserModule,
     FormsModule,
     CursosModule,
+    AlunosModule,
     AppRoutingModule
     //routing
   ],
