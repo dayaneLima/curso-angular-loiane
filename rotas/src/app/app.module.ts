5d85200a62c6f56cae40e09fa06217b6d7cbd8d3
Dayane <dayane.lima.castro@gmail.com>
2018-11-25 10:38:31 -0200

    62: Rotas: Dica de Performance: Carregamento sob demanda (lazy loading)

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -6,9 +6,9 @@ import { MaterializeModule } from 'angular2-materialize';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { CursosModule } from './cursos/cursos.module';
+// import { CursosModule } from './cursos/cursos.module';
 import { AppRoutingModule } from './app.routing.module';
-import { AlunosModule } from './alunos/alunos.module';
+// import { AlunosModule } from './alunos/alunos.module';
 
 // import { CursosComponent } from './cursos/cursos.component';
 // import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
@@ -28,8 +28,8 @@ import { AlunosModule } from './alunos/alunos.module';
   imports: [
     BrowserModule,
     FormsModule,
-    CursosModule,
-    AlunosModule,
+    // CursosModule,
+    // AlunosModule,
     AppRoutingModule
     //routing
   ],
