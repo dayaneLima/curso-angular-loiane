dc93dd0fde2412c0069d0d4d54bcc14edeee432c
Dayane <dayane.lima.castro@gmail.com>
2018-12-03 20:45:27 -0200

    77: Forms (template driven) Aplicando validação nos campos

--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -4,19 +4,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TemplateFormComponent } from './template-form/template-form.component';
+// import { TemplateFormComponent } from './template-form/template-form.component';
 import { DataFormComponent } from './data-form/data-form.component';
+import { TemplateFormModule } from './template-form/template-form.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TemplateFormComponent,
+    // TemplateFormComponent,
     DataFormComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TemplateFormModule
   ],
   providers: [],
   bootstrap: [AppComponent]
