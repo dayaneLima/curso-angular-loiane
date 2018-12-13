2133de9471181f53884307dbb719c092f8615fc2
Dayane <dayane.lima.castro@gmail.com>
2018-12-12 22:49:34 -0200

    86: Forms (template driven) Populando campos com setValue e patchValue (CEP)

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -35,16 +35,60 @@ export class TemplateFormComponent implements OnInit {
     }
   }
   
-  consultaCEP(cep){
+  consultaCEP(cep, formulario){
       cep = cep.replace(/\D/g, '');//somente dígitos
       if(cep != ""){
           var validacep = /^[0-9]{8}$/;//cep com 8 digitos numéricos
           if(validacep.test(cep)){
+            
+              this.resetaDadosForm(formulario);
+
               this.http.get(`//viacep.com.br/ws/${cep}/json`)
                 .pipe(map(dados => dados.json()))
-                .subscribe(dados => console.log(dados));
+                .subscribe(dados => this.populaDadosForm(dados, formulario));
           }
       }
+  }
+
+  populaDadosForm(dados, formulario){
+      //o nome e e-mail deve pegar do próprio formulario para não apagar
+      //tem que passar todas as propriedades do formulário
+      // formulario.setValue({
+      //     nome: formulario.value.nome,
+      //     email: formulario.value.email,
+      //     endereco: {
+      //         rua: dados.logradouro,
+      //         cep: dados.cep,
+      //         numero: "",
+      //         complemento: dados.complemento,
+      //         bairro: dados.bairro,
+      //         cidade: dados.localidade,
+      //         estado: dados.uf
+      //     }
+      // });
+    
+      //Passa somente os atributos que deseja alterar
+      formulario.form.patchValue({
+          endereco: {
+              rua: dados.logradouro,
+              complemento: dados.complemento,
+              bairro: dados.bairro,
+              cidade: dados.localidade,
+              estado: dados.uf
+          }
+      });      
+  }  
+
+  resetaDadosForm(formulario){
+      formulario.form.patchValue({
+          endereco: {
+              rua: null,
+              complemento: null,
+              bairro: null,
+              cidade: null,
+              estado: null
+          }
+      });
   }
 
 }
