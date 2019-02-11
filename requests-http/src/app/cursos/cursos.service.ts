f23c2287e2b2a577553662b511d0a05d67355be8
Dayane <dayane.lima.castro@gmail.com>
2019-02-11 20:22:18 -0200

    121: Http: Dica: Variável de Ambiente

--- requests-http/src/app/cursos/cursos.service.ts
+++ requests-http/src/app/cursos/cursos.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { environment } from '../../environments/environment';
 import { Curso } from './curso';
 
 @Injectable({
@@ -8,7 +9,7 @@ import { Curso } from './curso';
 })
 export class CursosService {
 
-  private readonly API = 'http://localhost:3000/cursos';
+  private readonly API = `${environment.API}cursos`;
 
   constructor(private http: HttpClient) { }
 
