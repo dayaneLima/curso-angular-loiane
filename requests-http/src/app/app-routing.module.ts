fd401987a5a14d03cdd232d3c4385024c714da64
Dayane <dayane.lima.castro@gmail.com>
2019-02-12 23:06:15 -0200

    123: Http + RxJS: Unsubscribe Automático

--- requests-http/src/app/app-routing.module.ts
+++ requests-http/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'cursos' },
   { path: 'cursos', loadChildren: './cursos/cursos.module#CursosModule' },
+  { path: 'rxjs-poc', loadChildren: './unsubscribe-rxjs/unsubscribe-rxjs.module#UnsubscribeRxjsModule' }
 ];
 
 @NgModule({
