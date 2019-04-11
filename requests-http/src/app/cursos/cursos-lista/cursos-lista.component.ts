c96d83f999816f2ac79c296ef4d913475990fde5
Dayane <dayane.lima.castro@gmail.com>
2019-04-11 20:48:22 -0300

    132: Http DELETE: Popup de Confirmação para remover Cursos

--- requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Observable, empty, Subject } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
@@ -15,12 +16,16 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class CursosListaComponent implements OnInit {
 
+  deleteModalRef: BsModalRef;
+  @ViewChild('deleteModal') deleteModal;
+
   // cursos: Curso[];
   cursos$: Observable<Curso[]>;
   error$ = new Subject<boolean>();
+  cursoSelecionado: Curso;
 
-  constructor(private service: CursosService, private alertService: AlertModalService, 
-    private router: Router, private route: ActivatedRoute) { }
+  constructor(private service: CursosService, private alertService: AlertModalService,
+    private router: Router, private route: ActivatedRoute, private modalService: BsModalService) { }
 
   ngOnInit() {
     // this.service.list()
@@ -60,6 +65,29 @@ export class CursosListaComponent implements OnInit {
 
   onEdit(id: number) {
     this.router.navigate(['editar', id], { relativeTo: this.route });
+  }
+
+  onDelete(curso: Curso) {
+    this.cursoSelecionado = curso;
+    this.deleteModalRef = this.modalService.show(this.deleteModal,  {class: 'modal-sm'});
+  }
+
+  onConfirmDelete() {
+    this.service.remove(this.cursoSelecionado.id)
+    .subscribe(
+      success => {
+        this.deleteModalRef.hide();
+        this.onRefresh();
+      },
+      error => {
+        this.deleteModalRef.hide();
+        this.alertService.showAlertDanger('Erro ao remover curso. Tente novamente mais tarde.');
+      }
+    );
+  }
+
+  onDeclineDelete() {
+    this.deleteModalRef.hide();
   }
 
 }
